fd97a018fe2ccd70cb516770485be3128fd43707
Totò Volturno <info@kunigu.tech>
2021-07-06 21:13:26 +0200

    bugs fixed

--- src/Dough.ts
+++ src/Dough.ts
@@ -6,12 +6,12 @@ import {
 } from "../generated/Dough/Dough"
 import { DoughHelper } from "../helpers/DoughHelper"
 
-export function handleTransfer(event: Transfer): void {
+export function handlerTransfer(event: Transfer): void {
   DoughHelper.transfer(event.params._from, event.params._to, event.params._amount);
 }
 
-export function handleClaimedTokens(event: ClaimedTokens): void {}
+export function handlerClaimedTokens(event: ClaimedTokens): void {}
 
-export function handleNewCloneToken(event: NewCloneToken): void {}
+export function handlerNewCloneToken(event: NewCloneToken): void {}
 
-export function handleApproval(event: Approval): void {}
+export function handlerApproval(event: Approval): void {}
--- src/Ypie.ts
+++ src/Ypie.ts
@@ -1,8 +1,64 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import { Ypie } from "../generated/Ypie/Ypie"
-import { Holder } from "../generated/schema"
+import { 
+  AnnualizedFeeSet,
+  Approval ,
+  Call,
+  CallerAdded,
+  CallerRemoved,
+  CapSet,
+  EntryFeeBeneficiaryShareSet,
+  EntryFeeSet,
+  ExitFeeBeneficiaryShareSet,
+  ExitFeeSet,
+  FeeBeneficiarySet,
+  FeeCharged,
+  LockSet,
+  OwnershipTransferred,
+  PoolExited,
+  PoolJoined,
+  TokenAdded,
+  TokenRemoved,
+  Transfer
+} from "../generated/Ypie/Ypie"
 
 /*
 handlerTransfer
   if address_from 0x0 -> MINT
 */
+
+export function handlerAnnualizedFeeSet(event: AnnualizedFeeSet): void {}
+
+export function handlerApproval(event: Approval): void {}
+
+export function handlerCallerAdded(event: CallerAdded): void {}
+
+export function handlerCallerRemoved(event: CallerRemoved): void {}
+
+export function handlerCapSet(event: CapSet): void {}
+
+export function handlerEntryFeeBeneficiaryShareSet(event: EntryFeeBeneficiaryShareSet): void {}
+
+export function handlerEntryFeeSet(event: EntryFeeSet): void {}
+
+export function handlerExitFeeBeneficiaryShareSet(event: ExitFeeBeneficiaryShareSet): void {}
+
+export function handlerExitFeeSet(event: ExitFeeSet): void {}
+
+export function handlerFeeBeneficiarySet(event: FeeBeneficiarySet): void {}
+
+export function handlerFeeCharged(event: FeeCharged): void {}
+
+export function handlerLockSet(event: LockSet): void {}
+
+export function handlerOwnershipTransferred(event: OwnershipTransferred): void {}
+
+export function handlerPoolExited(event: PoolExited): void {}
+
+export function handlerPoolJoined(event: PoolJoined): void {}
+
+export function handlerTokenAdded(event: TokenAdded): void {}
+
+export function handlerTokenRemoved(event: TokenRemoved): void {}
+
+export function handlerTransfer(event: Transfer): void {}
+
+export function handlerCall(event: Call): void {}
